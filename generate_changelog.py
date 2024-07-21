6d2f4429fa2f7a8271f17744cb9218f90a759dc9
DMKAL <83312683+tirzah2@users.noreply.github.com>
2024-07-21 20:03:36 +0100

    Create generate_changelog.py

---
+++ generate_changelog.py
@@ -0,0 +1,12 @@
+import json
+
+with open('releases.json', 'r') as file:
+    releases = json.load(file)
+
+changelog = ""
+for release in releases:
+    release_notes = release.get('body', '')
+    release_tag = release.get('tag_name', '')
+    changelog += f"## {release_tag}\n{release_notes}\n\n"
+
+print(changelog)
