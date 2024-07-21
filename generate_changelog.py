05d9f567da4e9b2ec9be172a3073ff34d6df7d25
DMKAL <83312683+tirzah2@users.noreply.github.com>
2024-07-21 20:05:31 +0100

    Update generate_changelog.py

--- generate_changelog.py
+++ generate_changelog.py
@@ -9,4 +9,5 @@ for release in releases:
     release_tag = release.get('tag_name', '')
     changelog += f"## {release_tag}\n{release_notes}\n\n"
 
-print(changelog)
+with open('changelog.txt', 'w') as file:
+    file.write(changelog)
